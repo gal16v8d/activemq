f4d06e47e8837333edc38443255f0f73998547fe
Alex Galvis <alex.galvis.sistemas@gmail.com>
2022-04-18 11:19:02 -0500

    Format missing file

--- src/main/java/com/gsdd/activemq/exception/ActiveMQEnvException.java
+++ src/main/java/com/gsdd/activemq/exception/ActiveMQEnvException.java
@@ -2,9 +2,9 @@ package com.gsdd.activemq.exception;
 
 public class ActiveMQEnvException extends RuntimeException {
 
-  private static final long serialVersionUID = -3125442528503490796L;
+    private static final long serialVersionUID = -3125442528503490796L;
 
-  public ActiveMQEnvException(String msg) {
-    super(msg);
-  }
+    public ActiveMQEnvException(String msg) {
+        super(msg);
+    }
 }
